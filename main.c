ee222fd4c5e4560529a67c9eaf74c426f65b021c
founder <djjotaremix@gmail.com>
2021-04-29 21:36:19 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,19 +2,32 @@
 // DEIS-ISEC 2020-2021
 // João Oliveira, nº 2018012875
 
+// Libraries
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+
 #include <stdbool.h>
+// decidi incluir o stdbool, pois é parte do standard C99 e eu sou dependente de boolean, sendo que é de boa prática
+// usá-lo para variáveis descartáveis :P
+
+//  #include <unistd.h>
+//  ^^  decidi não usar o access() para ficheiros, para não arriscar ter problemas com compilação noutros PCs,
+//      apesar de ser uma library standard de C.
+
+// Header Files
+
 #include "menus.h"
 // #include "utils.h"
 
 int JogoAnterior(){
-    const char *NomeSaveBin = "LastGameSave.bin";
-    if( access( NomeSaveBin, F_OK ) == 0 ) {
+    FILE * saveFile;
+    saveFile = fopen("LastGameSave.bin", "r"); // read-only. só vai verificar se o ficheiro existe.
+    if (saveFile){
         // ISTO SIGNIFICA QUE EXISTE UM JOGO ANTERIOR GUARDADO QUE NÃO FOI TERMINADO
+        fclose(saveFile);
         return 1;
-    } else {
+    }else{
         // ISTO SIGNIFICA QUE NÃO EXISTE NENHUM SAVE ANTERIOR OU QUE O JOGO ANTERIOR FOI TERMINADO
         return 0;
     }
@@ -49,18 +62,20 @@ int main() {
     int inputMenu = 0;
     bool avancar = false;
 
-    if(JogoAnterior() == 1){
+    int HaJogoAnterior = JogoAnterior();
+
+    if(HaJogoAnterior == 1){
         printf("\nDeixou um jogo por completar! Deseja continuar ou voltar para o menu?\n\n1 - Continuar\n2 - Voltar ao menu");
         scanf("%d", &inputMenu);
         if(inputMenu == 1) {
-
+            printf("\nGanda fixe!");
         }
         else if (inputMenu == 2) {
             menu;
         }
         else printf("\nInput invalido! Volte a tentar.");
     }
-    else if(JogoAnterior() == 0){
+    if(HaJogoAnterior == 0){
         menu;
     }
 
